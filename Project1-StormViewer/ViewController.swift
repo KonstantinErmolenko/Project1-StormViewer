2a6bfcf40d7c74797bfa62880d65a1312fd8f3c5
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-03-12 10:19:47 +0300

    Initial Commit

---
+++ Project1-StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1-StormViewer
+//
+//  Created by Ермоленко Константин on 12.03.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
