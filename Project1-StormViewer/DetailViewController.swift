95e4eed527a3d75da117797866d0025b0ca67406
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-03-15 22:20:19 +0300

    Complete project 3. Added share button at the Detail VC

--- Project1-StormViewer/DetailViewController.swift
+++ Project1-StormViewer/DetailViewController.swift
@@ -21,9 +21,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Picture \(selectedImageNumber) of \(totalImages)"
-        navigationItem.largeTitleDisplayMode = .never
-        
+        setupNavigationBar()
+
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -37,5 +36,28 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    // MARK: - Private Methods
+    private func setupNavigationBar() {
+        title = "Picture \(selectedImageNumber) of \(totalImages)"
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(shareTapped)
+        )
+    }
+    
+    @objc private func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }
